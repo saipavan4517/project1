361f5ab1adfc0f365d26462cdecae983740b46f0
saipavan4517 <39875373+saipavan4517@users.noreply.github.com>
2019-07-12 01:13:15 +0530

    Update Listcheck.java

--- project1test/src/project1test/Listcheck.java
+++ project1test/src/project1test/Listcheck.java
@@ -7,24 +7,24 @@ public class Listcheck {
 	public static void main(String[] args) {
 
 		int[] array ={9,5,52,63,7,52,19,7,5};
-		//int e;
-		LinkedList<Integer> mylist = new LinkedList<>();
-//		mylist.add(4);
-//		mylist.add(9);
 		
+		LinkedList<Integer> mylist = new LinkedList<>();
+
+
 		for(int i=0;i<=array.length;i++){
-			if(mylist.isEmpty()) {
-				//e = array[i];
+			if(mylist.isEmpty()) 
+			
 				mylist.add(array[i]);
-			}
+			
 			else if(mylist.contains(array[i]))
-			{
+			
 				System.out.println(" first repeated value is   "+ array[i]);
 				break;
-			} 
-			else {
+			}
+			else 
 				mylist.add(array[i]);
 			}
-			}}	}
+	                }	
+                        }
 
 
